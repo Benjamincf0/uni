9288d57f6e940fad4dcabe25b1663e8e544ee6c9
Benjamin Curis-Friedman <bencf99@gmail.com>
2026-09-21 01:05:07 -0400

    compose.ml

---
+++ COMP302/exercises/compose.ml
@@ -0,0 +1,17 @@
+
+let double x = 2 * x
+let increment x = x + 1
+
+(* 1a: double_add_one *)
+let double_add_one = compose increment double
+
+(* 1b: do_8_times *)
+let do_8_times f x =
+  let do_2_times x = (compose f f) x in
+  let do_4_times x =  x |> do_2_times |> do_2_times
+  in 
+  compose do_4_times do_4_times x
+
+
+(* 2: add_one_double *)
+let add_one_double x = x |> increment |> double
